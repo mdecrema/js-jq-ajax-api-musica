c8ab47db8d8121e2f6849bdda93feb9e6844153b
marco <marcodecrema@libero.it>
2020-09-17 16:28:51 +0200

    handlebar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
-	//Code
+
+
+  // PROVA
+
+    $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function (data, stato) {
+      var caratteristiche = data.response;
+      for (i=0; i<caratteristiche.length; i++) {
+        var autore = caratteristiche[i].author;
+        console.log(autore);
+      }
+    },
+    error: function (richiesta, stato, errori) {
+      alert("Errore: "+errori);
+    }
+  })
+
+
+  var cd = {
+    "cd": "cd",
+    "titolo": "title",
+    "autore": "author",
+    "anno": "year"
+  }
+
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+
+  var contenuto = template(cd);
+
+  $(".cds-container").append(contenuto);
+
+
+
 });
