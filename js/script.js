9447429a9bf59dcdb85033a13f95f1726c2ee319
marco <marcodecrema@libero.it>
2020-09-17 17:05:49 +0200

    link immagini

--- js/script.js
+++ js/script.js
@@ -8,23 +8,26 @@ $(document).ready(function() {
     method: "GET",
     success: function (data, stato) {
       var caratteristiche = data.response;
+      console.log(caratteristiche);
       /*for (i=0; i<caratteristiche.length; i++) {
         var autore = caratteristiche[i].author;*/
-      datiBrano(caratteristiche);
 
+      // Richiamo la funzione 'datiBrano' e gli passo l'argomento
+      datiBrano(caratteristiche);
     },
     error: function (richiesta, stato, errori) {
       alert("Errore: "+errori);
     }
   })
 
-
+// Funzione per intercettare i dati nei rispettivi oggetti
 function datiBrano(songs) {
   for (i=0; i<=songs.length; i++) {
     let canzone = songs[i];
 
     var cd = {
       "cd": "cd",
+      "img": canzone.poster,
       "author": canzone.author,
       "title": canzone.title,
       "year": canzone.year
